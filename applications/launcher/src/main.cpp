8a0ea9f7302e52de20480cbb2919d81f35f13455
Kojo Bailey <kojomolojo@gmail.com>
2026-08-16 18:54:15 +0100

    Make some quick clean-up

--- applications/launcher/src/main.cpp
+++ applications/launcher/src/main.cpp
@@ -1,27 +1,21 @@
 //
 // Created by kapil on 12.02.2026.
 //
+
 #include <windows.h>
-#include <filesystem>
-#include <fstream>
-#include <iostream>
 
 #include "binary_file.h"
-#include "crypt.h"
 #include "downloader.h"
 #include "logger.h"
-#include "process.h"
 
-#include "defines.h"
 #include "launcher.h"
 
 int main() {
-    // Init logger
     logger::init("JAPILauncher", "japi/logs");
 
     try {
-        launcher l;
-        l.run();
+        launcher{}
+			.run();
     } catch (const std::exception& e) {
         ERROR_AND_QUIT(e.what());
     }
